bdb5a288baf06d70601a633b99dbc09e65e5f03b
Oleg Dramaretskyi <dramarec@gmail.com>
2021-04-05 15:48:00 +0300

    adds initials files

---
+++ refs/os_ref.js
@@ -0,0 +1,23 @@
+const os = require('os')
+
+// Платформа
+console.log(os.platform())
+
+// Архитектура
+console.log(os.arch())
+
+// Информация 
+console.log(os.cpus())
+
+// Свободная память
+console.log(os.freemem())
+
+// Сколько всего памяти
+console.log(os.totalmem())
+
+// Корневая директория
+console.log(os.homedir())
+
+// Сколько система работает
+console.log(os.uptime())
+//node os_ref.js
